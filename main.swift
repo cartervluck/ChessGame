ec5209faf23aed4a91fcaa9c6816c7c10d750cd9
Carter Luck <carter.luck@djusdk12ca.org>
2016-10-15 00:04:55 +0000

    basic move func outline

--- main.swift
+++ main.swift
@@ -1,8 +1,8 @@
 class ChessPiece {
-	var color: Bool
-	var x: Int
-	var y: Int
-	var num: Int
+	var color: Bool = true
+	var x: Int = 0
+	var y: Int = 0
+	var num: Int = 0
 	var alive = true
 	init(color: Bool, x: Int, y: Int, num: Int){
 		self.color = color
@@ -13,15 +13,19 @@ class ChessPiece {
 	func isPossible(x: Int, y: Int, toX: Int, toY: Int, color: Bool) -> Bool {
 		return true
 	}
-	func move(x: Int, y: Int, toX: Int, toY: Int) -> (Int, Int, Bool, Int) {
-		return (0, 0, true, 0)
+	func move(x: Int, y: Int, toX: Int, toY: Int) {
+		if isPossible(x = x, y = y, toX = toX, toY = toY, color: turn % 2) {
+			//change board array
+		} else {
+			print("move not possible")
+		}
 	}
 }
 
 class Pawn: ChessPiece {
 	override func isPossible(x: Int, y: Int, toX: Int, toY: Int, color: Bool) -> Bool {
 		if color == true {
-			if turn == 1 {
+			if turn == 0 {
 				if toY == y + 1 || toY == y + 2 {
 					return true
 				}
@@ -31,7 +35,7 @@ class Pawn: ChessPiece {
 				}
 			}
 		} else {
-			if turn == 2 {
+			if turn == 1 {
 				if toY == y - 1 || toY == y - 2 {
 					return true
 				}
@@ -45,6 +49,8 @@ class Pawn: ChessPiece {
 	}
 }
 
-
+class Board {
+	var boardPieces: [[ChessPiece]] = [[],[var WP1: ChessPiece = Pawn(color = true, x = 0, y = 1, num = 1)]]
+}
 
 var turn = 1
